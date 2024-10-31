284fe749da137132fda03a9f962ecc53aa6021e9
IsaharStokhammer <8526656@gmail.com>
2024-10-31 09:40:45 +0200

    app.tsx בסיסי עם פורם בסיסי -פוקצליות ברמת בדיקת שפיות בלבד

---
+++ missions/src/components/missionForm/MissionForm.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Mission } from "../../types/types";
+
+interface MissionFormProps {
+  addMission: (mission: Mission) => Promise<void>;
+}
+
+const MissionForm: React.FC<MissionFormProps> = ({
+  addMission: addMission,
+}) => {
+  const [newMission, setNewMission] = useState<Mission | null>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (!newMission) {
+        console.log("no mission");
+        
+      return;
+    }
+    console.log(newMission);
+    
+    addMission(newMission);
+    setNewMission(null);
+  };
+  return (
+    <form className="mission-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        // onChange={(e) => setNewMission(e.target.value)}
+        placeholder="Add new Mission"
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default MissionForm;
---
+++ missions/src/types/types.ts
@@ -0,0 +1,13 @@
+export interface Mission{
+    name: string;
+    status: string;
+    priority: string;
+    description: string;
+}
+
+const exsampleMission = {
+    name: "Mission 1",
+    status: "In progress",
+    priority: "Medium",
+    description: "Mission 1 description",
+}
